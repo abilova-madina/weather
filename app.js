bc52ff963a677762e88cdbc083ff768dcbba4ed7
abilova-madina madi <abilovamadii@gmail.com>
2024-01-09 11:17:32 +0000

    app.js

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ $(document).on('keypress', function (e) {
     if (e.which == 13) {
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myweather").val()}&appid=48428a9b0f58392f6e8678ffaf133932&units=metric`;
         async function myweather() {
-            let responce = await fetch(url)
-            let data = await responce.json()
-            $("#city").html(data.name)
-            $("#temp").html(data.main.temp)
-            $("#feels").html(data.main.feels_like)
-            $("#myWeather").val("")
+            let responce = await fetch(url);
+            let data = await responce.json();
+            $("#city").html(data.name);
+            $("#temp").html(data.main.temp);
+            $("#feels").html(data.main.feels_like);
+            $("#myWeather").val("");
             console.log(data)
         }
         myweather()
@@ -21,12 +21,12 @@ $("#btn").click(function (e) {
     e.preventDefault()
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myweather").val()}&appid=48428a9b0f58392f6e8678ffaf133932&units=metric`;
     async function myweather() {
-        let responce = await fetch(url)
-        let data = await responce.json()
-        $("#city").html(data.name)
-        $("#temp").hmtl(data.main.temp)
-        $("#feels").html(data.main.feels_like)
-        $("myWeather").val("")
+        let responce = await fetch(url);
+        let data = await responce.json();
+        $("#city").html(data.name);
+        $("#temp").hmtl(data.main.temp);
+        $("#feels").html(data.main.feels_like);
+        $("myWeather").val("");
         console.log(data)
     }
     myweather()
